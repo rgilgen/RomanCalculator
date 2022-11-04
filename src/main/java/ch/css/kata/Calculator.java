a843fd85cc60b8dc7ccca5f2d442c38cb2331a69
adrian-sommer <adrian.sommer@css.ch>
2022-11-04 10:01:22 +0100

    Added first class and test

---
+++ src/main/java/ch/css/kata/Calculator.java
@@ -0,0 +1,8 @@
+package ch.css.kata;
+
+public class Calculator {
+
+    public String calculate(String romanNumber1, String romanNumber2) {
+        return null;
+    }
+}
---
+++ src/test/java/ch/css/kata/CalculatorTest.java
@@ -0,0 +1,14 @@
+package ch.css.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void  firstDummyTest() {
+        assertTrue(true);
+    }
+
+
+}
