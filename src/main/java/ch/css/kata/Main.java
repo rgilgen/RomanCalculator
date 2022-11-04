29d563e3be0269c0527543965904d30c6e9f51ba
adrian-sommer <adrian.sommer@css.ch>
2022-11-04 10:23:48 +0100

    Changes by resgilgen

--- src/main/java/ch/css/kata/Main.java
+++
@@ -1,7 +0,0 @@
-package ch.css.kata;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/test/java/ch/css/kata/CalculatorTest.java
+++ src/test/java/ch/css/kata/CalculatorTest.java
@@ -1,17 +1,20 @@
 package ch.css.kata;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     Calculator testee = new Calculator();
-    @Test
-    void  addIandIreturnsII() {
-        String result = testee.calculate("I", "I");
 
-        assertEquals("II", result);
+    @ParameterizedTest
+    @CsvSource({"I,I,II", "II,I,III"})
+    void  addIIandIreturnsIII(String number1 , String number2, String expectedResult) {
+        String result = testee.calculate(number1, number2);
+
+        assertEquals(expectedResult, result);
     }
-
 
 }
